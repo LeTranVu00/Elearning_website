78bf4ccb9212fe6c036f47f873ccaf01fe0fedbe
Hynnie72 <nhi722006@gmail.com>
2026-05-30 08:25:48 +0700

    Revert "Hoan thanh chuc nang login"

--- config/google_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$clientID = getenv('GOOGLE_CLIENT_ID');
-$clientSecret = getenv('GOOGLE_CLIENT_SECRET');
-
-define('GOOGLE_REDIRECT_URI', 'http://localhost/Elearning_website/pages/login.php');
-
-?>
